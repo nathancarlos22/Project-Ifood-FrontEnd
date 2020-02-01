721e6a5d077eb029d02c9a226606b482f52df9db
Nathan <nathangomes@mat.ci.ufpb.br>
2020-02-01 11:49:33 -0200

    Updates

---
+++ precache-manifest.1df3a5698e1e8e5b67a2ebcd9099df0a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb91ffcc9f1e6963bd8a20848632577b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7b8087170b115f1b2f61",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "40f8ccf16660fae5fe55",
+    "url": "/static/css/main.9a731346.chunk.css"
+  },
+  {
+    "revision": "7b8087170b115f1b2f61",
+    "url": "/static/js/2.269c3137.chunk.js"
+  },
+  {
+    "revision": "a52d65c95a2d7ffcb9ff212c4d38dff4",
+    "url": "/static/js/2.269c3137.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40f8ccf16660fae5fe55",
+    "url": "/static/js/main.e0c36743.chunk.js"
+  },
+  {
+    "revision": "aeb3b6e26438a9dea726",
+    "url": "/static/js/runtime-main.229f1a63.js"
+  },
+  {
+    "revision": "12fbc153eb142a5e54b30e3b4c3ec373",
+    "url": "/static/media/back-image.12fbc153.jpg"
+  }
+]);
