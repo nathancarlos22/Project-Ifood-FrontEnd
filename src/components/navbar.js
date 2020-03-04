7973343711787d9369434f14764c3ff4d58c4443
Neithan <nathangomes@mat.ci.ufpb.br>
2020-03-04 01:57:52 -0300

    requisição do cadastro

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,7 +1,41 @@
-import { Navbar, Form, FormControl, Button, Nav, NavDropdown, DropdownButton, Dropdown } from 'react-bootstrap';
+import { Nav, Dropdown } from 'react-bootstrap';
 import React, {Component} from 'react';
-
+const axios = require('axios');
 export default class navbar extends Component {
+
+    constructor(props) {
+        super(props);
+        this.handleChange = this.handleChange.bind(this);
+        this.cadaUser = this.cadaUser.bind(this);
+
+        this.state = {
+            firstName: '',
+            lastName:'',
+            email: '',
+            password: '',
+            adress_id:'1'
+
+        }
+    }
+    handleChange(e){
+        this.setState({ [e.target.name]: e.target.value });
+    }
+
+    cadaUser() { /* Cadastra */
+        var first_name = document.querySelector('#Nome1').value;
+        var last_name= document.querySelector('#Nome2').value;
+        var email = document.querySelector('#DropdownFormEmail1').value;
+        var password = document.querySelector('#DropdownFormPassword1').value;
+        var adress_id=1;
+     
+        axios.post('http://localhost:3000/users',{first_name, last_name, email, password, adress_id})
+        .then(() => {
+            alert("sucesso ao cadastrar");
+        }).catch((err) => {
+            alert("error: " + err.toString());
+        });
+    }
+    
     render () {
         return (
             <Nav className="justify-content" id="mynav">
@@ -13,28 +47,29 @@ export default class navbar extends Component {
                     
                         <Dropdown.Menu className="dropdown-menu">
                             <form className="px-4 py-3">
-                                <div class="form-group">
-                                    <label for="exampleDropdownFormEmail1">Endereço de email</label>
-                                    <input type="email" class="form-control" id="exampleDropdownFormEmail1" placeholder="email@exemplo.com"/>
-                                </div>
                                 <div className="form-group">
-                                    <label for="exampleDropdownFormPassword1">Senha</label>
-                                    <input type="password" class="form-control" id="exampleDropdownFormPassword1" placeholder="Senha"/>
+                                    <label htmlFor="exampleDropdownFormEmail1">Endereço de email</label>
+                                    <input type="email" className="form-control" id="exampleDropdownFormEmail1" placeholder="email@exemplo.com"/>
                                 </div>
+
+                                <div className="form-group">
+                                    <label htmlFor="exampleDropdownFormPassword1">Senha</label>
+                                    <input type="password" className="form-control" id="exampleDropdownFormPassword1" placeholder="Senha"/>
+                                </div>
+
                                 <div className="form-check">
-                                    <input type="checkbox" class="form-check-input" id="dropdownCheck"/>
-                                    <label className="form-check-label" for="dropdownCheck">
+                                    <input type="checkbox" className="form-check-input" id="dropdownCheck"/>
+                                    <label className="form-check-label" htmlFor="dropdownCheck">
                                         Remember me
                                     </label>
                                 </div>
+
                                 <button type="submit" className="btn btn-primary">Entrar</button>
                             </form>
-                            
                         </Dropdown.Menu>
                     </div>
-
-                    
                 </Dropdown>
+
                 <Dropdown>
                     <div className="btn">
                             <Dropdown.Toggle>
@@ -42,32 +77,34 @@ export default class navbar extends Component {
                             </Dropdown.Toggle>
                         
                             <Dropdown.Menu className="dropdown-menu-cadastro">
-                                <form className="px-4 py-3">
-                                    <div class="form-group">
-                                        <label for="DropdownNome">Nome Completo</label>
-                                        <input class="form-control" id="Nome" placeholder="Coloque seu nome completo"/>
-                                    </div>
-                                    <div class="form-group">
-                                        <label for="DropdownNome">Endereço</label>
-                                        <input class="form-control" id="Nome" placeholder="Coloque seu endereço"/>
-                                    </div>
-                                    <div class="form-group">
-                                        <label for="DropdownFormEmail1">Endereço de email</label>
-                                        <input type="email" class="form-control" id="DropdownFormEmail1" placeholder="email@exemplo.com"/>
-                                    </div>
+                                   
+                            <form className="px-4 py-3">
                                     <div className="form-group">
-                                        <label for="DropdownFormPassword1">Senha</label>
-                                        <input type="password" class="form-control" id="DropdownFormPassword1" placeholder="Senha"/>
+                                        <label htmlFor="DropdownNome1">Primeiro Nome</label>
+                                        <input className="form-control" id="Nome1" placeholder="Coloque seu nome completo"/>
                                     </div>
                                     
-                                    <button type="submit" className="btn btn-primary">Cadastrar-se</button>
+                                    <div className="form-group">
+                                        <label htmlFor="DropdownNome2">Segundo Nome </label>
+                                        <input className="form-control" id="Nome2" placeholder="Coloque seu nome completo"/>
+                                    </div>                                  
+
+                                    <div className="form-group">
+                                        <label htmlFor="DropdownFormEmail1">Endereço de email</label>
+                                        <input type="email" className="form-control" id="DropdownFormEmail1" placeholder="email@exemplo.com"/>
+                                    </div>
+
+                                    <div className="form-group">
+                                        <label htmlFor="DropdownFormPassword1">Senha</label>
+                                        <input type="password" className="form-control" id="DropdownFormPassword1" placeholder="Senha"/>
+                                    </div>
+                                    
+                                    <button type="submit" onClick={this.cadaUser} className="btn btn-primary">Cadastrar-se</button>
                                 </form>
-                                
                             </Dropdown.Menu>
                     </div>
                     </Dropdown>
              </Nav>                    
-
         );
     }
 }
